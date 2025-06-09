69c63564e1784c70d7879889404028a66ef5a6e0
foreseenlime <troyhkiwi@gmail.com>
2025-06-09 12:45:11 +1200

    changed sidebar behaviour + added content in news page

---
+++ circle-click/c.js
@@ -0,0 +1,15 @@
+// variables
+let score = 0;
+let time = 20;
+
+// elements
+const start_btn = document.getElementById("start-game");
+const title = document.getElementById("title");
+
+// checks for game start button clicked
+start_btn.addEventListener("click", startGame);
+
+function startGame() {
+    // yep
+    
+}
