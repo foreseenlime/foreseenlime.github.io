b4d0aa2be12481a6a8b59e33d2bc75e5d1933924
foreseenlime <troyhkiwi@gmail.com>
2026-09-24 17:00:40 +1200

    fully finished rpm calculator page, started on a team notes page

--- robotics_resources/calculator.js
+++ robotics_resources/calculator.js
@@ -1,77 +1,91 @@
 // div elements for iteration
-const input_gears_div = document.getElementById("input_gears");
-const output_gears_div = document.getElementById("output_gears");
-const input_rpm_div = document.getElementById("input_rpm");
-const wheel_size_div = document.getElementById("wheel_size");
+const input_gears_div = document.getElementById("input_gears")
+const output_gears_div = document.getElementById("output_gears")
+const input_rpm_div = document.getElementById("input_rpm")
+const wheel_size_div = document.getElementById("wheel_size")
 
 // button elements
-const input_gears_buttons = input_gears_div.querySelectorAll("button");
-const output_gears_buttons = output_gears_div.querySelectorAll("button");
-const input_rpm_buttons = input_rpm_div.querySelectorAll("button");
-const wheel_size_buttons = wheel_size_div.querySelectorAll("button");
+const input_gears_buttons = input_gears_div.querySelectorAll("button")
+const output_gears_buttons = output_gears_div.querySelectorAll("button")
+const input_rpm_buttons = input_rpm_div.querySelectorAll("button")
+const wheel_size_buttons = wheel_size_div.querySelectorAll("button")
 
 // display elements
-const ratio_display = document.getElementById("ratio_display");
-const rpm_display = document.getElementById("rpm_display");
-const speed_display = document.getElementById("speed_display");
+const ratio_display = document.getElementById("ratio_display")
+const rpm_display = document.getElementById("rpm_display")
+const speed_display = document.getElementById("speed_display")
 
 // button variables
-var input_teeth;
-var output_teeth;
+var input_teeth
+var output_teeth
 
-var input_rpm;
-var wheel_size;
+var input_rpm
+var wheel_size
 
 // calculated variables
-var ratio;
-var output_rpm;
-var lin_speed;
+var ratio
+var output_rpm
+var lin_speed
 
-const trunc = 100;
-var circum;
+const trunc = 100
+var circum
 
 function truncate(num) {
-    return (Math.round(num * trunc)) / trunc;
+    return (Math.round(num * trunc)) / trunc
 }
 
-input_gears_buttons.forEach((item) => {
-    item.addEventListener("click", () => {
-        input_teeth = parseInt(item.innerHTML);
-    });
-});
+function remove_active(elements, item) {
+    elements.forEach(btn => {
+        if(btn.classList.contains("active")) {
+            btn.classList.remove("active")
+        }
+    })
+    item.classList.add("active")
+}
 
-output_gears_buttons.forEach((item) => {
+input_gears_buttons.forEach(item => {
     item.addEventListener("click", () => {
-        output_teeth = parseInt(item.innerHTML);
-    });
-});
+        input_teeth = parseInt(item.innerHTML)
+        remove_active(input_gears_buttons, item)
+    })
+})
 
-input_rpm_buttons.forEach((item) => {
+output_gears_buttons.forEach(item => {
     item.addEventListener("click", () => {
-        input_rpm = parseInt(item.innerHTML);
-    });
-});
+        output_teeth = parseInt(item.innerHTML)
+        remove_active(output_gears_buttons, item)
+    })
+})
 
-wheel_size_buttons.forEach((item) => {
+input_rpm_buttons.forEach(item => {
     item.addEventListener("click", () => {
-        wheel_size = parseFloat(item.innerHTML);
-    });
-});
+        input_rpm = parseInt(item.innerHTML)
+        remove_active(input_rpm_buttons, item)
+    })
+})
+
+wheel_size_buttons.forEach(item => {
+    item.addEventListener("click", () => {
+        wheel_size = parseFloat(item.innerHTML)
+        remove_active(wheel_size_buttons, item)
+    })
+})
 
 document.addEventListener("click", () => {
     if(input_teeth != undefined && output_teeth != undefined && input_rpm != undefined) {
         // gear ratio
-        ratio = truncate(input_teeth / output_teeth);
-        ratio_display.innerHTML = `1 : ${ratio}`;
+        ratio = truncate(input_teeth / output_teeth)
+        ratio_display.innerHTML = `1 : ${ratio}`
 
         // rp
-        output_rpm = truncate(input_rpm * ratio);
-        rpm_display.innerHTML = `${output_rpm} rpm`;
+        output_rpm = truncate(input_rpm * ratio)
+        rpm_display.innerHTML = `${output_rpm} rpm`
 
         if(wheel_size != undefined) {
-            circum = Math.PI * wheel_size;
-            lin_speed = truncate((output_rpm * circum) / 60);
-            speed_display.innerHTML = `${lin_speed} inches/s`;
+            circum = Math.PI * wheel_size
+            lin_speed = truncate((output_rpm * circum) / 60)
+            speed_display.innerHTML = `${lin_speed} inches/s`
         }
     }
-});
+})
+
